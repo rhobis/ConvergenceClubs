92e732eb4eeb4875576bc27f48aa18a675a25dbb
Roberto S <rhobis@gmail.com>
2017-11-13 18:31:25 +0100

    added data countryGDP

---
+++ R/countryGDP.R
@@ -0,0 +1,19 @@
+#' GDP of NUTS regions from 200x to 20xx.......
+#'
+#' A dataset containing the GDP of 268 NUTS regions over 15 years.
+#' Data were filtered in order  to remove business cycles.
+#'
+#'
+#' \describe{
+#'   \item{ID}{Country (character);}
+#'   \item{Y1970, ..., Y2003}{GDP from year 1970 to 2003 (filtered in order
+#'    to remove business cycles).}
+#' }
+#'
+#'
+#' @docType data
+#' @keywords datasets
+#' @name countryGDP
+#' @usage data(countryGDP)
+#' @format A data frame with 152 rows and 35 variables:
+NULL
