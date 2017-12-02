52656ac2da8ce144af5f89032acbee0d50c4e991
Roberto S <rhobis@gmail.com>
2017-12-02 02:12:36 +0100

    unified estimateMod methods in one function

--- R/estimateMod.R
+++ R/estimateMod.R
@@ -88,3 +88,50 @@ estimateMod <- function(H, time_trim, HACmethod = c('FQSB','AQSB')){
     }else stop("An error occurred, check the value of HACmethod!")
 }
 
+
+
+### ----------------------------------------------------------------------------
+
+#' Long run variance of errors
+#'
+#' Estimate long run variance of errors by Andrews method
+#'
+#' @param x vector of residuals
+#'
+#' @return  a numeric value representing the long run variance of errors
+#'
+#'
+#' @keywords internal
+
+
+### Function taken from ...
+andrs2		<- function(x){
+    t		<- length(x[, 1])
+    n		<- length(x[1, ])
+    x1		<- x[1:(t - 1), ]
+    y1		<- x[2:t, ]
+    b1		<- sum(x1*y1)/sum(x1^2)
+    ee		<- y1 - x1*b1
+
+    a1		<- (4*b1^2)/(((1 - b1)^2)*((1 + b1)^2))
+    a2		<- (4*b1^2)/((1 - b1)^4)
+    band1	<- 1.1447*(a1*t)^(1/3)
+    band2	<- 1.3221*(a2*t)^(1/5)
+    jb2		<- as.matrix((1:(t - 1))/c(band2))
+    jband2	<- jb2*1.2*pi
+    kern1	<- ((sin(jband2)/jband2 - cos(jband2))/((jb2*pi)^2*12))*25
+
+    tt		<- length(ee)
+    lam		<- as.matrix(0)
+    for(j in 1:(tt - 1)){
+        ttp1	<- (t(x[1:(tt - j), ]) %*% x[(1 + j):tt, ])*(as.matrix(kern1[j, ]/tt))[1, 1]
+        ttp	<- t(t(x[1:(tt - j), ]) %*% x[(1 + j):tt, ])*as.matrix(kern1[j, ]/tt)[1, 1]
+        lam	<- lam + ttp + ttp1
+    }
+    sigm	<- (t(x) %*% x)/as.matrix(tt)
+    lam		<- sigm[1] + lam
+
+    return (lam)
+}
+
+
