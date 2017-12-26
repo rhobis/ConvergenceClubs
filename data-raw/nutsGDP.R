8feed9f2bfcfe54583c4c804b3e5324c3fd4c920
Roberto S <rhobis@gmail.com>
2017-12-26 12:28:08 +0100

    removed unused data

--- data-raw/nutsGDP.R
+++
@@ -1,4 +0,0 @@
-nutsGDP <- read.table("data-raw/GDPfiltered.txt", header=T, stringsAsFactors = FALSE)
-colnames(nutsGDP)[2:16] <- paste('Y', 2000:2014, sep='')
-
-devtools::use_data(nutsGDP,overwrite=TRUE)
