a65c01abe3330861794ae5b0cfe3eafe56bc1efe
Roberto S <rhobis@gmail.com>
2017-12-26 16:57:50 +0100

    re-organized code for model estimation

--- R/estimateMod.R
+++ R/estimateMod.R
@@ -16,7 +16,7 @@
 #' @return  A list containing information about the model used to run the t-test
 #' on the regions in the club: beta coefficient, standard deviation, t-statistics and p-value.
 #'
-#'@details The following linear model is estimated:
+#' @details The following linear model is estimated:
 #' \deqn{\log\frac{H_1}{H_t} – 2\log(\log{t}) = \alpha + \beta \log{t} + u_t}{
 #'            log[H(1)/H(t)] – 2log[log(t)] = \alpha + \beta log(t) + u(t)  }
 #'
@@ -36,7 +36,8 @@
 #' Econometrica: Journal of the Econometric Society, 817-858.
 #'
 #'
-#' @importFrom stats lm pnorm setNames
+#' @importFrom stats lm pnorm setNames coef model.matrix residuals
+#'
 #'
 #' @export
 #'
@@ -46,90 +47,35 @@ estimateMod <- function(H, time_trim, HACmethod = c('FQSB','AQSB')){
 
     HACmethod <- match.arg(HACmethod)
 
-    if (HACmethod=='FQSB'){
-        ### Initialise variables -----------------------------------------------
-        nT <- length(H)
-        rT <- seq( round(nT*time_trim) + 1, nT)
-        logt <- log(rT)
-        rH <- log( H[1]/H[rT] ) - 2*log( logt )
-        ### Estimation ---------------------------------------------------------
-        xx		<- cbind(1, logt)						# construct design matrix
-        b		<- solve( t(xx) %*% xx ) %*% t(xx) %*% rH	# OLS
-        re		<- rH - xx %*% b						# construct residuals
+    ### Initialise variables -----------------------------------------------
+    nT <- length(H)
+    rT <- seq( round(nT*time_trim) + 1, nT)
+    logt <- log(rT)
+    rH <- log( H[1]/H[rT] ) - 2*log( logt )
 
-        lrv		<- andrs2(re)              				#long-run variance of errors
-        var.b	<- diag( solve(t(xx) %*% xx) )*c(lrv)
-        se.b	<- sqrt(var.b)
-        tstat.b	<- b[2]/se.b[2]
-        result	<- list(beta=b,
-                       st.dev = se.b[2],
-                       tvalue = tstat.b,
-                       pvalue = pnorm( q=tstat.b )
-        )
-        ### Output -------------------------------------------------------------
-        return(result)
-        #
-        #
-    }else if(HACmethod=="AQSB"){
-        ### Initialise variables -----------------------------------------------
-        nT <- length(H)
-        rT <- seq( round(nT*time_trim) + 1, nT)
-        logt <- log(rT)
-        rH <- log( H[1]/H[rT] ) - 2*log( logt )
+    if (HACmethod=='FQSB'){
         ### Estimation ---------------------------------------------------------
         mod <- lm( rH~logt )
-        mod <- lmtest::coeftest(mod,vcov=sandwich::vcovHAC(mod))
+        mm <- model.matrix(mod)
+        r <- residuals(mod)
+        hac <- ps_andrews_hac(r)
+        se <- sqrt( diag( solve( t(mm) %*% mm) ) * hac )[2]
+        b <- coef(mod)[2]
+        tv <- b/se
         ### Output -------------------------------------------------------------
-        return(list(beta= mod[2,1],
-                    st.dev = mod[2,2],
-                    tvalue = mod[2,3],
-                    pvalue = mod[2,4]))
+        return(list(beta= b,
+                    st.dev = se,
+                    tvalue = tv,
+                    pvalue = pnorm(q=tv) )
+        )
+    }else if(HACmethod=="AQSB"){
+        ### Estimation ---------------------------------------------------------
+        mod <- lm( rH~logt )
+        out <- lmtest::coeftest(mod,vcov=sandwich::vcovHAC(mod))
+        ### Output -------------------------------------------------------------
+        return(list(beta= out[2,1],
+                    st.dev = out[2,2],
+                    tvalue = out[2,3],
+                    pvalue = out[2,4]))
     }else stop("An error occurred, check the value of HACmethod!")
 }
-
-
-
-### ----------------------------------------------------------------------------
-
-#' Long run variance of errors
-#'
-#' Estimate long run variance of errors by Andrews method
-#'
-#' @param x vector of residuals
-#'
-#' @return  a numeric value representing the long run variance of errors
-#'
-#'
-#' @keywords internal
-
-
-### Function taken from ...
-andrs2		<- function(x){
-    t		<- length(x[, 1])
-    x1		<- x[1:(t - 1), ]
-    y1		<- x[2:t, ]
-    b1		<- sum(x1*y1)/sum(x1^2)
-    ee		<- y1 - x1*b1
-
-    a1		<- (4*b1^2)/(((1 - b1)^2)*((1 + b1)^2))
-    a2		<- (4*b1^2)/((1 - b1)^4)
-    band1	<- 1.1447*(a1*t)^(1/3)
-    band2	<- 1.3221*(a2*t)^(1/5)
-    jb2		<- as.matrix((1:(t - 1))/c(band2))
-    jband2	<- jb2*1.2*pi
-    kern1	<- ((sin(jband2)/jband2 - cos(jband2))/((jb2*pi)^2*12))*25
-
-    tt		<- length(ee)
-    lam		<- as.matrix(0)
-    for(j in 1:(tt - 1)){
-        ttp1	<- (t(x[1:(tt - j), ]) %*% x[(1 + j):tt, ])*(as.matrix(kern1[j, ]/tt))[1, 1]
-        ttp	<- t(t(x[1:(tt - j), ]) %*% x[(1 + j):tt, ])*as.matrix(kern1[j, ]/tt)[1, 1]
-        lam	<- lam + ttp + ttp1
-    }
-    sigm	<- (t(x) %*% x)/as.matrix(tt)
-    lam		<- sigm[1] + lam
-
-    return (lam)
-}
-
-
---
+++ R/ps_andrews_hac.R
@@ -0,0 +1,50 @@
+#' Long run variance of errors
+#'
+#' Estimate long run variance of errors through Andrews' method
+#'
+#' @param x vector of residuals
+#'
+#' @return  a numeric value representing the long run variance of errors
+#'
+#'
+#' @details This function computes the long run variance of residuals of an lm model
+#'     by means of Andrews' with Quadratic Spectral kernel and fixed bandwidth.
+#'     The code is an adaptation of Phillips and Sul (2007)'s code,
+#'     which was written in \emph{GAUSS}.
+#'
+#' @references
+#' Phillips, P. C.; Sul, D., 2007.
+#' Transition modeling and econometric convergence tests. Econometrica 75 (6), 1771-1855.
+#'
+#'
+#' @keywords internal
+
+
+ps_andrews_hac <- function(x){
+    t <- length(x)
+    x1 <- x[ seq(1, t-1) ]
+    x2 <- x[ seq(2, t) ]
+    b1 <- sum(x1*x2)/sum(x1**2)
+    ee <- x2 - x1*b1
+    ## compute alpha
+    a <- (4 * b1**2) / ((1-b1)**4)
+    ## Compute bandwidth
+    B <- 1.3221 * (a*t)**0.2
+    l <- seq_len(t-1)
+    z <- 1.2*pi * l/B
+    ## Compute kernel
+    k <- (sin(z)/z - cos(z)) * (3/z**2)
+
+    ## Compute vcov matrix
+    t2 <- t-1
+    out <- 0
+    for(i in seq_len(t2-1)){
+        o <- (t(x[seq(1,t2-i)]) %*% x[seq(i+1, t2)] ) * k[i]/t2
+        o1 <- t(t(x[seq(1,t2-i)]) %*% x[seq(i+1, t2)] ) * k[i]/t2
+        out <- out + o + o1
+    }
+    s <- t(x) %*% x / t2
+    out <- c(out + s)
+    #
+    return(out)
+}
