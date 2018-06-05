7b554829eb5ee58b7ee458b8bea5e72d32dff7ca
Roberto S <rhobis@gmail.com>
2018-06-04 23:52:51 -0400

    added save option

--- R/plot.convergence.clubs.R
+++ R/plot.convergence.clubs.R
@@ -5,6 +5,16 @@
 #'@param nrows number of rows of the graphical layout, if NULL, it is automatically defined
 #'@param ncols number of columns of the graphical layout, if NULL, it is automatically defined
 #'@param y_fixed logical, should the scale of the y axis be the same for all plots?
+#'@param save logical, should the plot be saved as a file?
+#'@param filename optional, a string indicating the name of the file where the plot
+#'    should be saved; must include the extension (e.g. "plot.pdf")
+#'@param path optional, a string representing the path of the directory where the plot should
+#'    saved; the path should not contain the a final slash symbol ("/")
+#'@param width the width of the plot, in inches.
+#'@param height the height of the plot, in inches.
+#'@param device string indicating the format to be used to save the plot;
+#'    one of "pdf", "png" or "jpeg".
+#'@param res the resolution of the image, in ppi; only used with \code{device="png"} and \code{device="jpeg"}
 #'@param ... other parameters to pass to function \code{plot()}.
 #'
 #'
@@ -31,11 +41,37 @@
 #'
 #'@export
 #'
-#'@importFrom grDevices n2mfrow
+#'@importFrom grDevices n2mfrow pdf png jpeg dev.off
 #'@importFrom graphics par matplot abline
 
 
-plot.convergence.clubs <- function(x, y = NULL, nrows=NULL, ncols=NULL, y_fixed = FALSE, ...){
+
+plot.convergence.clubs <- function(x,
+                                   y = NULL,
+                                   nrows=NULL,
+                                   ncols=NULL,
+                                   y_fixed = FALSE,
+                                   save = FALSE,
+                                   filename,
+                                   path,
+                                   width = 7,
+                                   height = 7,
+                                   device = c("pdf", "png", "jpeg"),
+                                   res,
+                                   ...){
+
+    ### Check input and initialise values ---
+
+    if( any( !is.null(nrows) & !is.numeric(nrows),
+             !is.null(ncols) & !is.numeric(ncols) ) )
+        stop("nrows and ncols must be either NULL or an integer scalar!")
+    if( !is.logical(y_fixed) ) y_fixed <- FALSE
+    if( !is.logical(save) ) save <- FALSE
+    if( missing(path) ) path <- getwd()
+    if( !file.exists(path) ) path <- dir.create(path, recursive=TRUE)
+    if( any( !is.numeric(width), !is.numeric(height) ) ) stop("width and height must be numeric scalars!")
+    if( !all( width>0, height>0) ) stop("width and height must be positive scalars!")
+    device <- match.arg(device)
 
     divergent <- !is.null( x$divergent )
     nclub <- dim(x)[1]
@@ -54,11 +90,6 @@ plot.convergence.clubs <- function(x, y = NULL, nrows=NULL, ncols=NULL, y_fixed 
 
 
     ### Compute dimensions plot layout ---
-    if( any( !is.null(nrows) & !is.numeric(nrows),
-             !is.null(ncols) & !is.numeric(ncols) ) )
-        stop("nrows and ncols must be either NULL or an integer scalar!")
-
-
     if( !is.null(nrows) & !is.null(ncols) ){
 
         pm <- floor( c(nrows, ncols) )
@@ -81,7 +112,41 @@ plot.convergence.clubs <- function(x, y = NULL, nrows=NULL, ncols=NULL, y_fixed 
     }else  pm <- grDevices::n2mfrow(nplots)
 
 
-    ### Generate plots ---
+    ### Generate/save plots  ---
+    ### Save plot ---
+    if( save ){
+
+        if( identical(device, 'pdf') ){
+            if( missing(filename) ) filename <- "Rplot%03d.pdf"
+
+            pdf(file = file.path(path, filename),
+                width=width,
+                height=height
+            )
+        }else if( identical(device, 'png') ){
+            if( missing(filename) ) filename <- "Rplot%03d.png"
+            if( missing(res) ) res <- 300
+
+            png(filename = file.path(path, filename),
+                width=width,
+                height=height,
+                units='in',
+                res=res
+            )
+        }else{
+            if( missing(filename) ) filename <- "Rplot%03d.jpeg"
+            if( missing(res) ) res <- 300
+
+            jpeg(filename = file.path(path, filename),
+                 width=width,
+                 height=height,
+                 units='in',
+                 res=res
+            )
+        }
+    }
+
+
     i <- 1
     par( mfrow=pm )
     while( i < prod(pm) & i < nplots ) {
@@ -97,6 +162,10 @@ plot.convergence.clubs <- function(x, y = NULL, nrows=NULL, ncols=NULL, y_fixed 
              ylab="Relative transition path", main = "All Clubs" )
     abline(h=1, lwd=2, col="black")
     par( mfrow=c(1,1) )
+
+
+    if( save ) dev.off()
+
 
 }
 
